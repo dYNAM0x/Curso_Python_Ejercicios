670455bef1fabfc1b1e29fb603a579e8c09cdae0
Alex LP <alex_lima_p@hotmail.com>
2022-06-16 18:12:50 -0500

    realizando ejercicio 2

---
+++ Operadores/Ejercicio 2 determinar l la solucionógica de la operaciion.py
@@ -0,0 +1,12 @@
+#ejercicio 2
+
+'''
+Determinar solucion lógica de ((3 + 5 * 8)< 3 and (-6/3 * 4) + 2 < 2)) or (a>b)
+'''
+
+a = float(input("Ingresa valor de a: "))
+b = float(input("Ingresa valor de b: "))
+
+resultado = ((3 + 5 * 8)< 3 and ((-6/3 * 4) + 2 < 2)) or (a>b)
+
+print(f"El resultado logico de la función es: {resultado}")

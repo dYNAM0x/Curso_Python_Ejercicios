36cd2aab91aa8c5f225c88826ec87b1e23a56912
Alex LP <alex_lima_p@hotmail.com>
2022-06-16 20:54:41 -0500

    corrjiendo ejercicio

--- Cap. 2 Condicionales/Ejercicio 1 numeros pares o impares.py
+++ Cap. 2 Condicionales/Ejercicio 1 numeros pares o impares.py
@@ -1,15 +1,15 @@
 #Ejercicio 1
 
-num1 = float(input("ingrese el primer numero: "))
-num2 = float(input("ingrese el segundo numero: "))
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
 
 
-if((num1 <= 0)  and (num2 <= 0)):
+if((num1 == 0)  and (num2 == 0)):
     print("son valores incorrectos: ERROR 404 NOT FOUND %=&)(/)$=$")
 elif (num1 % 2 == 0 and num2 % 2 == 0):
     print("ambos numeros son pares")
-elif(num1 % 2 ==0):
-    print("el primer numero es par")
-elif(num2 % 2 == 0):
-    print("el segundo numero es par")
+elif(num1 % 2 ==0 and num2 % 2 != 0):
+    print(f"el par es {num1}")
+elif (num1 % 2 != 0 and num2 % 2 == 0):
+    print(f"el par es {num2}")
 else: print("ninguno es par")

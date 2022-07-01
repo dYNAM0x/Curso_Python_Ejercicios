ac155f23d22bb1e55406f2667b245173eba27199
Alex LP <alex_lima_p@hotmail.com>
2022-07-01 16:29:03 -0500

    agregando teoria de tuplas

---
+++ Cap3 Colecciones/tuplas.py
@@ -0,0 +1,16 @@
+#Tuplas
+'''
+Las tuplas son listas pero que no pueden ser modificadas
+no se pueden borrar sus datos, ni tampoco se pueden añadir
+'''
+
+
+'''
+las tuplas se puede modificar, convirtiendolas en listas
+'''      #0 1 2 3 4
+tupla1 = (1,2,3,4,5)
+lista = list(tupla1)
+
+print(6 in tupla1)
+print(5 in tupla1)
+print(tupla1[4])
